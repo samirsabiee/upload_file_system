aec3bd64103c1cec53f5f27974825e22768f5ce6
samsab1995 <samirsabiee@gmail.com>
2021-08-31 02:29:13 +0430

    add new func and constructor

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -3,16 +3,31 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\FileRequest;
+use App\Services\Uploader\StorageManager;
+use App\Services\Uploader\Uploader;
+use Illuminate\Http\RedirectResponse;
 
 class FileController extends Controller
 {
+    private Uploader $uploader;
+
+    /**
+     * FileController constructor.
+     * @param Uploader $uploader
+     */
+    public function __construct(Uploader $uploader)
+    {
+        $this->uploader = $uploader;
+    }
+
     public function create()
     {
         return view('files.create');
     }
 
-    public function new(FileRequest $request)
+    public function new(FileRequest $request): RedirectResponse
     {
-        dd($request->file);
+        $this->uploader->upload();
+        return redirect()->back()->with('success', 'File Uploaded Successfully');
     }
 }
