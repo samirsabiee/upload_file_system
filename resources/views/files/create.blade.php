f5c00ceffe51d752f453dc26b0a498068b614613
samsab1995 <samirsabiee@gmail.com>
2021-08-31 01:16:46 +0430

    upload file blade

---
+++ resources/views/files/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row d-flex flex-row justify-content-center align-items-center">
+        <div class="col-5 card p-0">
+            <div class="card-header">Upload File</div>
+            <div class="card-body">
+                <form method="post" action="{{ route('file.new') }}">
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text" id="title_span">Upload</span>
+                        </div>
+                        <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="file" aria-describedby="file input">
+                            <label class="custom-file-label" for="file">Choose file</label>
+                        </div>
+                    </div>
+                    <div class="custom-control custom-checkbox mb-3">
+                        <input type="checkbox" class="custom-control-input" id="private">
+                        <label class="custom-control-label" for="private">Will Upload Private</label>
+                    </div>
+                    <button class="btn btn-primary" type="submit">Upload File</button>
+                </form>
+            </div>
+        </div>
+    </div>
+@endsection
