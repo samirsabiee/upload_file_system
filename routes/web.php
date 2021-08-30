5359a988e520de3c236a7a2cb489e71e9507abfa
samsab1995 <samirsabiee@gmail.com>
2021-08-31 01:18:31 +0430

    reformat code and add file routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::view('/', 'welcome');
+Route::get('file/create',[FileController::class, 'create'])->name('file.create');
+Route::post('file',[FileController::class, 'new'])->name('file.new');
+
+
