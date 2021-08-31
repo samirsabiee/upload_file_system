975c59463473d63823f5adb452e0d4d040153b16
samsab1995 <samirsabiee@gmail.com>
2021-08-31 18:50:08 +0430

    add funcs to StorageManager.php

--- app/Services/Uploader/StorageManager.php
+++ app/Services/Uploader/StorageManager.php
@@ -4,6 +4,7 @@
 namespace App\Services\Uploader;
 
 
+use Illuminate\Contracts\Filesystem\Filesystem;
 use Illuminate\Http\UploadedFile;
 use Storage;
 
@@ -17,5 +18,25 @@ class StorageManager
     public function putFileAsPublic(string $name, UploadedFile $file, string $type)
     {
         return Storage::disk('public')->putFileAs($type, $file, $name);
+    }
+
+    function getAbsolutePathOf(string $name, string $type, bool $private): string
+    {
+        return $this->disk($private)->path($this->directoryPrefix($name, $type));
+    }
+
+    public function isFileExist(string $name, string $type, bool $isPrivate): bool
+    {
+        return $this->disk($isPrivate)->exists($this->directoryPrefix($name, $type));
+    }
+
+    private function directoryPrefix(string $name, string $type): string
+    {
+        return $type . DIRECTORY_SEPARATOR . $name;
+    }
+
+    private function disk(bool $private): Filesystem
+    {
+        return $private ? Storage::disk('private') : Storage::disk('public');
     }
 }
