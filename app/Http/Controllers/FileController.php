bba6efa4c1c56552f6937b95e55517217336b120
samsab1995 <samirsabiee@gmail.com>
2021-08-31 18:48:38 +0430

    add try-catch block

--- app/Http/Controllers/FileController.php
+++ app/Http/Controllers/FileController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Exceptions\FileHasExistException;
 use App\Http\Requests\FileRequest;
 use App\Services\Uploader\StorageManager;
 use App\Services\Uploader\Uploader;
+use Exception;
 use Illuminate\Http\RedirectResponse;
 
 class FileController extends Controller
@@ -27,7 +29,13 @@ class FileController extends Controller
 
     public function new(FileRequest $request): RedirectResponse
     {
-        $this->uploader->upload();
-        return redirect()->back()->with('success', 'File Uploaded Successfully');
+        try {
+            $this->uploader->upload();
+            return redirect()->back()->with('success', 'File Uploaded Successfully');
+        } catch (FileHasExistException $e) {
+            return redirect()->back()->with('error', 'File Exist can\'t upload again');
+        } catch (Exception $e) {
+            return redirect()->back()->with('error', $e->getMessage());
+        }
     }
 }
