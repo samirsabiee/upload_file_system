c062989bf1e7cca11b70f7162a8740c4ff0758cb
samsab1995 <samirsabiee@gmail.com>
2021-08-31 18:50:42 +0430

    complete upload file and check if exist

--- app/Services/Uploader/Uploader.php
+++ app/Services/Uploader/Uploader.php
@@ -4,40 +4,80 @@
 namespace App\Services\Uploader;
 
 
-use GuzzleHttp\Psr7\UploadedFile;
+use App\Exceptions\FileHasExistException;
+use App\Models\File;
 use Illuminate\Http\Request;
 
 class Uploader
 {
     private Request $request;
     private StorageManager $storageManager;
+    private FFMpegService $ffmpeg;
     private $file;
 
     /**
      * Uploader constructor.
      * @param Request $request
      * @param StorageManager $storageManager
+     * @param FFMpegService $ffmpeg
      */
-    public function __construct(Request $request, StorageManager $storageManager)
+    public function __construct(Request $request, StorageManager $storageManager, FFMpegService $ffmpeg)
     {
         $this->request = $request;
         $this->storageManager = $storageManager;
         $this->file = $request->file;
+        $this->ffmpeg = $ffmpeg;
+
     }
 
-
+    /**
+     * @throws FileHasExistException
+     */
     public function upload()
     {
+        if ($this->isFileExist()) throw new FileHasExistException();
         $this->putFileIntoStorage();
+        $this->saveFileIntoDatabase();
+    }
+
+    private function saveFileIntoDatabase()
+    {
+        $file = new File([
+            'name' => $this->file->getClientOriginalName(),
+            'size' => $this->file->getSize(),
+            'type' => $this->getType(),
+            'is_private' => $this->isPrivate()
+        ]);
+        $file->time = $this->getTime($file);
+        $file->save();
+    }
+
+    private function getTime(File $file)
+    {
+        if (!$file->isMedia()) return null;
+
+        return $this->ffmpeg->durationOf($file->absolutePath());
     }
 
     private function putFileIntoStorage()
     {
-        $method = $this->request->has('private') ? 'putFileAsPrivate' : 'putFileAsPublic';
+        $method = $this->isPrivate() ? 'putFileAsPrivate' : 'putFileAsPublic';
         $this->storageManager->$method($this->file->getClientOriginalName(), $this->file, $this->getType());
     }
 
-    private function getType(): string
+    private function isFileExist(): bool
+    {
+        return $this->storageManager->isFileExist($this->file->getClientOriginalName(), $this->getType(), $this->isPrivate());
+    }
+
+    private
+    function isPrivate(): bool
+    {
+        return $this->request->has('is_private');
+    }
+
+    private
+    function getType(): string
     {
         return [
             'image/jpeg' => 'image',
