14fef5c07b1e0c5ce718981cbcc530a2dd843a19
samsab1995 <samirsabiee@gmail.com>
2021-09-01 08:55:33 +0430

    add form post for delete file

--- resources/views/files/index.blade.php
+++ resources/views/files/index.blade.php
@@ -2,38 +2,40 @@
 
 @section('content')
     <div class="row d-flex flex-column justify-content-center align-items-center">
-        <form action="#" method="post" class="col-8">
-            <table class="table table-hover table-bordered">
-                <thead>
+
+        <table class="table table-hover table-bordered col-8">
+            <thead>
+            <tr>
+                <th>Name</th>
+                <th>Type</th>
+                <th>Size</th>
+                <th>Time</th>
+                <th>Access</th>
+                <th>Operation</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($files as $file)
                 <tr>
-                    <th>Name</th>
-                    <th>Type</th>
-                    <th>Size</th>
-                    <th>Time</th>
-                    <th>Access</th>
-                    <th>Operation</th>
-                </tr>
-                </thead>
-                <tbody>
-                @foreach($files as $file)
-                    <tr>
-                        <td>{{ $file->name }}</td>
-                        <td>{{ $file->type }}</td>
-                        <td>{{ $file->toMegabyteSize() }} Megabyte</td>
-                        <td>{{ $file->time }}</td>
-                        @if($file->is_private)
-                            <td>private</td>
-                        @else
-                            <td>public</td>
-                        @endif
-                        <td>
-                            <a href="{{ route('file.show', $file->id) }}" class="btn btn-sm btn-primary">Download</a>
+                    <td>{{ $file->name }}</td>
+                    <td>{{ $file->type }}</td>
+                    <td>{{ $file->toMegabyteSize() }} Megabyte</td>
+                    <td>{{ $file->time }}</td>
+                    @if($file->is_private)
+                        <td>private</td>
+                    @else
+                        <td>public</td>
+                    @endif
+                    <td>
+                        <a href="{{ route('file.show', $file->id) }}" class="btn btn-sm btn-primary">Download</a>
+                        <form action="{{ route('file.delete',$file->id) }}" method="post" class="d-inline">
+                            @csrf
                             <button class="btn btn-sm btn-primary">Delete</button>
-                        </td>
-                    </tr>
-                @endforeach
-                </tbody>
-            </table>
-        </form>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
